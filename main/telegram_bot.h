d07ef4f5acaa8cc00f722dcfdf0600c06306c956
Vanik Tigranyan <90699514+vaniktigranyan@users.noreply.github.com>
2025-09-01 16:48:05 +0400

    Update telegram_bot.h

--- main/telegram_bot.h
+++ main/telegram_bot.h
@@ -1,3 +1,10 @@
+
+typedef struct {
+    char bot_token[128];        // Telegram bot token
+    char api_url[128];          // Base API URL (e.g., "https://api.telegram.org")
+} telegram_bot_info_t;
+
+
 void http_test_task(void *pvParameters);
 void check_bot_info(void);
 void send_telegram_message(const char* chat_id, const char* message_text);
