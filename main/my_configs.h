04bf508f2ca5f4bbed1d9c827413d1febde9e400
vaniktigranyan <vanik.tigranyan@gmail.com>
2025-09-05 01:17:56 +0400

    root id

--- main/my_configs.h
+++ main/my_configs.h
@@ -4,12 +4,12 @@
 
 
 /* WI-FI configuration*/
-#define WIFI_SSID      "Arpinet-49312"
-#define WIFI_PASS      "Avo94773122"
+#define WIFI_SSID      "-----"
+#define WIFI_PASS      "----"
 
 /*Telegram configuration*/
-#define TOKEN "8177540145:AAGbkafQeWWIO_JayCA_6kyuamVD8GXIAp0"
+#define TOKEN "---------"
 #define ALLOWED_MAX_CHAT_IDS 10
-#define ROOT_CHAT_ID "1329988285"
+#define ROOT_CHAT_ID "------"
 
 #define GET_NEW_MSG_TIME 2000 //in ms
